1e69579e71017477ff8c82a99df3223ef9cff7a3
mimiban <dianabanuelos122@gmail.com>
2025-12-13 22:27:53 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install Python GPIO library
+sudo apt-get install python3-rpi.gpio -y
+
+# Install Tesseract OCR (text recognition)
+sudo apt-get install tesseract-ocr -y
+
+# Install audio playback tool
+sudo apt-get install alsa-utils -y
+
+# Install camera utilities
+sudo apt-get install libcamera-apps -y
